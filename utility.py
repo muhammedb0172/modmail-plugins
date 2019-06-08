0fa33a1a61d7a7a46e7aa837efd3dec9b407c652
muhammedb0172 <35309216+muhammedb0172@users.noreply.github.com>
2019-06-08 18:03:37 +0200

    Update utility.py

--- utility.py
+++ utility.py
@@ -11,7 +11,7 @@ class UtilityExamples(commands.Cog):
     @commands.command()
     async def say(self, ctx, *, message: commands.clean_content):
         """Repeats after you"""
-        await ctx.send(message)
+        await ctx.send("Message")
         await message.delete(1)
 
     @commands.group(invoke_without_command=True)
