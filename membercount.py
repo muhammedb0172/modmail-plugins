64094735d1c6ebcb53a29d2cc954d6ce46c3d2a9
muhammedb0172 <35309216+muhammedb0172@users.noreply.github.com>
2019-06-09 18:24:14 +0200

    Update membercount.py

--- membercount.py
+++ membercount.py
@@ -1,4 +1,33 @@
-mc = ctx.guild.member_count
-em = discord.Embed(color=bot.main_color, description=mc, title="Current Members")
-em.set_footer(text="This is how many members we have currently!")
-await ctx.send(embed=em)
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+Cog = getattr(commands, 'Cog', object)
+
+
+class Member Count(Cog):
+    """Information plugin"""
+    
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def membercount(self, ctx):
+        """How many members there are in this server."""
+
+        mc = ctx.guild.member_count
+        embed = discord.Embed(
+            title="Members",
+            color=discord.Color.red(),
+            description = "mc amount of people are in the server!"
+        )
+        embed.add_field(name="Links",value="Coming soon!")
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Information(bot))
