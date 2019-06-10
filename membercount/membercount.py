923cf8a0373ec31bac856f49590917d8333877bc
muhammedb0172 <35309216+muhammedb0172@users.noreply.github.com>
2019-06-10 12:50:58 +0200

    Update membercount.py

--- membercount/membercount.py
+++ membercount/membercount.py
@@ -7,24 +7,25 @@ from core.models import PermissionLevel
 Cog = getattr(commands, 'Cog', object)
 
 
-class Membercount(Cog):
-    """Shows you the amount of people that are in the current server!"""
+class Serverinfo(Cog):
+    """Info of your current server!"""
     
     def __init__(self, bot):
         self.bot = bot
     
     @commands.command()
     @checks.has_permissions(PermissionLevel.REGULAR)
-    async def membercount(self, ctx):
-        """Shows you how many members there are in this server."""
+    async def serverinfo(self, ctx):
+        """Shows you a little info your server!"""
 
         mc = ctx.guild.member_count
         embed = discord.Embed(
-            title="Members",
+            title="Serverinfo!",
             color=discord.Color.red(),
             description = f"{mc} amount of people are in the server!"
         )
         embed.add_field(name="Q: Who made this?",value="A: By AshyHi#2158")
+        embed.add_field(name="Are there coming more stuff?",value="Yes! Ofcourse! But you just need to wait!")
         embed.add_field(name="Links?",value="Please wait. And update the bot daily and use the command to when the link is here!")
         await ctx.send(embed=embed)
 
